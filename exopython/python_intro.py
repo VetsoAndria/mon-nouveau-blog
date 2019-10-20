becce00e61b40db9ba7b7a5cd8ae8acb883c7f70
VetsoRajao <vetso@ymail.com>
2019-10-20 11:50:18 +0200

    Mon app Django Girls, premier commit

---
+++ exopython/python_intro.py
@@ -0,0 +1,20 @@
+def hi():
+	print('Hi there!')
+	print('How are you?')
+
+hi()
+
+def hi(name):
+    if name == 'Ola':
+    	print('Hi Ola!')
+    elif name == 'Sonja':
+    	print('Hi Sonja!')
+    else:
+    	print('Hi anonymous!')
+
+hi()	
+
+
+
+
+
